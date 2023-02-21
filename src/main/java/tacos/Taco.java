d17726464b9a46f5709cdf7074f0b6ee2d38fd8d
lyubo92 <lyuboslavvarbanov@gmail.com>
2023-02-21 21:38:31 +0200

    created JPA entities, Repositories

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,26 +1,30 @@
-// tag::all[]
-// tag::allButValidation[]
 package tacos;
+import java.util.Date;
 import java.util.List;
 // end::allButValidation[]
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 // tag::allButValidation[]
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
 
-  // end::allButValidation[]
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
+  private Long id;
   @NotNull
   @Size(min=5, message="Name must be at least 5 characters long")
-  // tag::allButValidation[]
   private String name;
-  // end::allButValidation[]
+  private Date createdAt;
+  @ManyToMany(targetEntity=Ingredient.class)
   @Size(min=1, message="You must choose at least 1 ingredient")
-  // tag::allButValidation[]
-  private List<String> ingredients;
+  private List<Ingredient> ingredients;
+  @PrePersist
+  void createdAt() {
+    this.createdAt = new Date();
+  }
 
 }
-//end::allButValidation[]
-//tag::end[]
